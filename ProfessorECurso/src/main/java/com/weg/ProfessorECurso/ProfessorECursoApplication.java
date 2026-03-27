dc9ce62e2ebed443b99a0f157053026f72e9a040
Vinicim <vinicius_anacleto@estudante.sesisenai.org.br>
2026-03-27 18:03:23 -0300

    Add files via upload

---
+++ ProfessorECurso/src/main/java/com/weg/ProfessorECurso/ProfessorECursoApplication.java
@@ -0,0 +1,13 @@
+package com.weg.ProfessorECurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorECursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorECursoApplication.class, args);
+	}
+
+}
